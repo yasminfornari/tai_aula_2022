d8755e3589409b326bd6729bf9f9048ace76f2e0
yasminfornari <94840794+yasminfornari@users.noreply.github.com>
2022-04-19 09:33:36 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    include "./database/bd.php"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $objBD = new BD();
+    $objBD->conn();
+    $result = $objBD->select();
+    foreach ($result as $item){
+        echo "ID".$item['id']." Nome: ".$item['nome'] ."<br>";
+    }
+?>
+</body>
+</html>
